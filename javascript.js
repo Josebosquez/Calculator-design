2195ae87a93fb8e395f5bd36bb724d58f81295af
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-24 15:48:56 -0600

    small update

--- javascript.js
+++ javascript.js
@@ -1,78 +1,69 @@
 // TODO: write query selectors and add event listeners to the calculator's buttons
-const keyNumbers = document.querySelectorAll(".num");
-
-for (const value of keyNumbers){
-    value.addEventListener('click', function (){
-        firstNumber+= value.innerText;
-        console.log(value.innerText)
-    });
-} 
-
-
 const keyOperators = document.querySelectorAll(".operator");
-
-for (const element of keyOperators){
-    element.addEventListener('click', function (){
-        console.log("it works")
-    });
-} 
-
-
-
-// const keyMinus = document.querySelector("#minus");
-// const keyMultiply = document.querySelector("#multiply");
-// const keyDivide = document.querySelector("#divide");
-const keyEqual = document.querySelector("#equalSign")
-const screens = document.querySelector("#screen")
+const keyNumbers = document.querySelectorAll(".num");
+const clear = document.querySelector("#clear");
+const screenBox = document.querySelector("#screen");
+const equalSign = document.querySelector("#equalSign");
+// grab the Num from that target.
 
 let firstNumber = '';
 let operation = '';
 let secondNumber = '';
-let screen = "";
-
-// grab the Num from that target.
-
+let screen = 0;
 
 
 // Calculates the result of the current expression if it is valid, then displays the result on the screen
+sum = 
 function calcResult() {
     // TODO
+    sum = firstNumber; operation; secondNumber
 }
+// const equals = document.querySelector(".equalSign");
+//     equals.addEventListener("click", calcResult )
+//     console.log("your great")
 
 // Handles when an operation button is pressed (+, -, /, *)
 
-// const operators = "keyDivide, keyPlus, keyMinus, keyMultiply"; 
-// function operationPressed(op) {
-//     // TODO
-//     let sum = ""
-// ;    if (operator === "+"){
-//         sum = firstNumber + secondNumber;
-//     }
-//     if (operator === "-"){
-//         sum = firstNumber - secondNumber;
-//     }
-//     if (operator === "/"){
-//         sum = firstNumber / secondNumber;
-//     }
-//     if (operator === "x"){
-//         sum = firstNumber * secondNumber;
-//     }
-// }
+let operationVariable = "";
+function operationPressed(op) {
+}
 
 // Handles when a number button is pressed
 
     // TODO
-//     key1.addEventListener("click", function (){
-//         console.log("you clicked one")
-// })
+    for (const element of keyOperators){
+        element.addEventListener("click", function (){
+            console.log(element.innerText)
+        });
+    } 
+
+    for (const value of keyNumbers){
+        value.addEventListener('click', function (){
+            firstNumber += value.innerText;
+            screen += firstNumber;
+            console.log(value.innerText)
+        });
+    } 
+
+    equalSign.addEventListener("click", function (){
+        screen = firstNumber; operation; secondNumber;
+    });
+
+    
+
 
 // Clears the screen
-const reset = document.getElementById("#C");
 function clearScreen() {
     // TODO
-    reset.addEventListener("click", function(){
-});
+    clear.addEventListener("click", function(){
+        firstNumber = "";
+        secondNumber = "";
+        operation = "";
+        screen = 0;
+    });
 }
+
+
 
 // Updates the screen based on `firstNumber`, `operation`, and `secondNumber`
 function updateScreen() {
